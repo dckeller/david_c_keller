c88ed1386eec6e0c8a02507e18815b9c30ddb19e
David Keller <dckeller1@gmail.com>
2017-05-05 09:09:19 -0700

    Add in driver code, array to hold calculations

--- week_5_assesment/calculator.rb
+++ week_5_assesment/calculator.rb
@@ -12,3 +12,17 @@ def calculator(option, val1, val2)
 		puts "Sorry, does not compute"
 	end 
 end 
+
+puts "Welcome to the Calculator program!"
+puts "Would you like to make a calculation? If yes enter 'yes' if not enter 'done'"
+continue = gets.chomp
+while continue != 'done'
+calc_array = []
+	puts "Great, lets continue"
+	puts "--------------------------------"
+	puts "Please give me a number:"
+	val1 = gets.chomp.to_i
+	puts "Please give me another number:"
+	val2 = gets.chomp.to_i
+	puts "Would you like to +, -, *, or /"
+	option = gets.chomp.to_s
